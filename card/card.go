1e8f32974f74eb5d67484cd6131c7c57ac4d890c
Souleiman <souleiman@hxdcml.com>
2014-08-21 00:17:51 -0400

    derp, indentation

--- card/card.go
+++ card/card.go
@@ -3,19 +3,19 @@ package card
 type Card struct {
 	Extra
 
-	Id         int`json:"multiverseid"` //The multiverseid of the card on Wizard's Gatherer web page.
-	Name       string`json:"name"`      //The card name. For split, double-faced and flip cards, just the name of one side of the card.
-	Names      []string`json:"names"`   //Only used for split, flip and dual cards. Will contain all the names on this card, front or back.
-	image      string`json:"imageName"` //The mtgimage.com file name for this card.
-	Type       string`json:"type"`         // The card type. This is the type you would see on the card if printed today. Uses UTF-8 u"\u2014"
-	Supertypes []string`json:"supertypes"` //The supertypes of the card. These appear to the far left of the card type.
-	Types      []string`json:"types"`      //The types of the card. These appear to the left of the dash in a card type.
-	Subtypes   []string`json:"subtypes`    //The subtypes of the card. These appear to the right of the dash in a card type.
-	Mana       string`json:"manaCost"` //The mana cost of this card.
-	Cost       float64`json:"cmc"`     // Converted mana cost.
-	Text       string`json:"text"`   // The text of the card.
-	Flavor     string`json:"flavor"` //The flavor text of the card.
-	Layout      string`json:"layout"` //The card layout. Possible values: normal, split, flip, double-faced, token, plane, scheme, phenomenon, leveler, vanguard
+	Id          int`json:"multiverseid"`    //The multiverseid of the card on Wizard's Gatherer web page.
+	Name        string`json:"name"`         //The card name. For split, double-faced and flip cards, just the name of one side of the card.
+	Names       []string`json:"names"`      //Only used for split, flip and dual cards. Will contain all the names on this card, front or back.
+	image       string`json:"imageName"`    //The mtgimage.com file name for this card.
+	Type        string`json:"type"`         // The card type. This is the type you would see on the card if printed today. Uses UTF-8 u"\u2014"
+	Supertypes  []string`json:"supertypes"` //The supertypes of the card. These appear to the far left of the card type.
+	Types       []string`json:"types"`      //The types of the card. These appear to the left of the dash in a card type.
+	Subtypes    []string`json:"subtypes`    //The subtypes of the card. These appear to the right of the dash in a card type.
+	Mana        string`json:"manaCost"`     //The mana cost of this card.
+	Cost        float64`json:"cmc"`         // Converted mana cost.
+	Text        string`json:"text"`         // The text of the card.
+	Flavor      string`json:"flavor"`       //The flavor text of the card.
+	Layout      string`json:"layout"`       //The card layout. Possible values: normal, split, flip, double-faced, token, plane, scheme, phenomenon, leveler, vanguard
 }
 
 type Extra struct {
--- card/set.go
+++ card/set.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Set struct {
-	Name        string`json:"name"`                    // The name of the set
-	Code        string`json:"code"`                    // The code name of the set
-	Block       string`json:"block"`                   // The block this set is in
-	release     string`json:"releaseDate"`             // When the set was released (YYYY-MM-DD)
-	RawCards    json.RawMessage`json:"cards"`          // Maintain the raw integrity of the card in json format
-	Cards       []Card                                 // The cards in the set in Object format
+	Name        string`json:"name"`           // The name of the set
+	Code        string`json:"code"`           // The code name of the set
+	Block       string`json:"block"`          // The block this set is in
+	release     string`json:"releaseDate"`    // When the set was released (YYYY-MM-DD)
+	RawCards    json.RawMessage`json:"cards"` // Maintain the raw integrity of the card in json format
+	Cards       []Card                        // The cards in the set in Object format
 }
 
 func (set Set) Transform() {
