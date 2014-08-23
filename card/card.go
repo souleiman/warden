7fc09a9dab2919929c1f92810a89a9efec91219c
Souleiman <souleiman@hxdcml.com>
2014-08-22 22:55:47 -0400

    separated card into two parts. BaseCard supporting single entities, whilst Card supports both single and multi entities

--- card/card.go
+++ card/card.go
@@ -1,15 +1,11 @@
 package card
 
-type Card struct {
+type BaseCard struct {
 	Id            int`json:"multiverseid"`              //The multiverseid of the card on Wizard's Gatherer web page.
 	Name          string`json:"name"`                   //The card name. For split, double-faced and flip cards, just the name of one side of the card.
 	Ascii         string`json:"ascii"`                  //The card name in ASCII format
-	Names         *[]string`json:"names"`               //Only used for split, flip and dual cards. Will contain all the names on this card, front or back.
 	image         *string`json:"imageName"`             //The mtgimage.com file name for this card.
 	Type          string`json:"type"`                   // The card type. This is the type you would see on the card if printed today. Uses UTF-8 u"\u2014"
-	Types         []string`json:"types"`                //The types of the card. These appear to the left of the dash in a card type.
-	SuperTypes    *[]string`json:"supertypes"`          //The supertypes of the card. These appear to the far left of the card type.
-	SubTypes      *[]string`json:"subtypes`             //The subtypes of the card. These appear to the right of the dash in a card type.
 	Mana          *string`json:"manaCost"`              //The mana cost of this card.
 	Cost          *float64`json:"cmc"`                  // Converted mana cost.
 	Text          *string`json:"text"`                  // The text of the card.
@@ -20,9 +16,18 @@ type Card struct {
 	Loyalty       *int`json:"loyalty"`                  //The loyalty of the card. This is only present for planeswalkers.
 	Hand          *int`json:"hand"`                     // Maximum hand size modifier. Only exists for Vanguard cards.
 	Life          *int`json:"life"`                     // Starting life total modifier. Only exists for Vanguard cards.
+}
+
+type Card struct {
+	BaseCard
+
+	Names         *[]string`json:"names"`               //Only used for split, flip and dual cards. Will contain all the names on this card, front or back.
+	Types         []string`json:"types"`                //The types of the card. These appear to the left of the dash in a card type.
+	SuperTypes    *[]string`json:"supertypes"`          //The supertypes of the card. These appear to the far left of the card type.
+	SubTypes      *[]string`json:"subtypes`             //The subtypes of the card. These appear to the right of the dash in a card type.
 	Colors        *[]string`json:"colors"`              //The card colors.
-	Legalities    *map[string]string`json:"legalities"` //Which formats this card is legal, restricted or banned in.
 	Rulings       *[]ruling`json:"rulings"`             //Each Ruling object has 'date' and 'text' keys.
+	Legalities    *map[string]string`json:"legalities"` //Which formats this card is legal, restricted or banned in.
 	Printings     *[]string`json:"printings"`           //The sets that this card was printed in.
 }
 
